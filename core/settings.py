29940b7a71e62c2ebb61ce2608255ec4388b0bfe
Arth26 <arthuralves@ads.fiponline.edu.br>
2026-07-22 17:13:34 -0300

    fix: Settings.py

--- core/settings.py
+++ core/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', '').split(',')]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
-CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')]
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 
 # Application definition
